96be9e6d07c2dcc6f8bfa1b48c906769a36d8aad
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-25 00:14:50 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Asilbek");
+  const [events, setEvents] = useState([
+    { title: "Asilbek", birthday: 2008, id: 1 },
+    { title: "Bexruz", birthday: 2012, id: 2 },
+    { title: "Azizbek", birthday: 2010, id: 3 },
+  ]);
+
+  const delClick = (id) => {
+    const filteredEvents = events.filter((event) => event.id !== id);
+    setEvents(filteredEvents);
+  };
+
+  const handleClick = () => {
+    setName("Bobur");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>salom mening ismim {name}</h1>
+      <button onClick={handleClick} className="bg-orange-600">
+        {" "}
+        Click me
+      </button>
+      {events.map((event) => {
+        return (
+          <div key={event.id}>
+            <h2>{event.title}</h2>
+            <button onClick={() => delClick(event.id)} className="">
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
